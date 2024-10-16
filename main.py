6681fc850c7b88196cab8234426a7a46a621dc76
ibaragozz <baragozz1@gmail.com>
2024-10-16 15:58:14 +0300

    end Lesson

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+    #Ввод переменной в URL с выводом на экран
+# @app.route('/')
+# @app.route('/<name>/')
+# def hello_world(name='Незнакомец'):
+#     return f'Привет, {name}!'
 
-@app.route('/newpage/')
-@app.route('/new/')
-@app.route('/новаястраница/')
-def new():
-    return 'New page'
+
+    #Переход на новую страницу по разным URL
+# @app.route('/newpage/')
+# @app.route('/new/')
+# @app.route('/новаястраница/')
+# def new():
+#     return 'New page'
+
+    #Проверка паролей
+# @app.route('/')
+# @app.route('/<password>/')
+# def check_pass(password=None):
+#     if password == '1234':
+#         return 'Password is correct'
+#     else:
+#         return 'Password is not correct'
+
+
+    #Вывод на экран с помощью HTML
+# @app.route('/')
+# def html():
+#     html = """
+#     <h1>Добро пожаловать</h1>
+#     <p>на сайт</p>
+#     """
+#     return html
+
+@app.route('/')
+def html():
+
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run()
