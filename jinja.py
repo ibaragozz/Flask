8c47fe16951bd13291eb4a43ef062bdebe021680
ibaragozz <baragozz1@gmail.com>
2024-10-17 14:53:46 +0300

    edit jinja

--- jinja.py
+++ jinja.py
@@ -2,35 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-
-
 @app.route('/')
-def html():
-    # context = {
-    #     'caption' : 'Наши в космосе',
-    #     'list' : ['Alex', 'Barbie', 'Vasya', 'Petya']
-    # }
-    context = {
-        "poem": [
-            "Сижу за решёткой в темнице сырой.",
-            "Вскормленный в неволе орёл молодой,",
-            "Мой грустный товарищ, махая крылом,",
-            "Кровавую пищу клюёт под окном,",
-            "Клюёт, и бросает, и смотрит в окно,",
-            "Как будто со мною задумал одно."]
+def index():
+    caption = {
+        'link' : 'Ссылка'
     }
+    return render_template('index.html', **caption)
 
-    return render_template('shablon.html', **context)
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
-@app.route('/shablon/')
-def html2():
-    context = {
-        'caption': 'Юра, мы все... живем в космосе',
-        'link': 'Подробнее'
-    }
-
-    return render_template('index.html', **context)
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == '__main__':
     app.run()
