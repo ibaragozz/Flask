0579931222af5135f977684fb954205b65443807
ibaragozz <baragozz1@gmail.com>
2024-10-17 14:01:47 +0300

    edit jinja, add shablon

--- jinja.py
+++ jinja.py
@@ -7,10 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def html():
+    context = {
+        'caption' : 'Наши в космосе',
+        'link' : 'Переход на сайт'
+    }
 
-    return render_template('index.html')
+    return render_template('index.html', **context)
 
+@app.route('/shablon/')
+def html2():
+    context = {
+        'caption': 'Юра, мы все... живем в космосе',
+        'link': 'Подробнее'
+    }
 
+    return render_template('index.html', **context)
 
 if __name__ == '__main__':
     app.run()
